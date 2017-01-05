7d1c828ff13e2567a68150c8fc6b9bd5717772d8
Henry Williams <hwill12345@gmail.com>
2017-01-05 16:14:27 -0500

    gyro code and lcd display

--- Greyfur/main.c
+++ Greyfur/main.c
@@ -50,6 +50,10 @@ float rightTarget = 0;
 float liftTarget = 0;
 
 
+bool gyroTurningActive = false;
+float gyroTarget = 0, speed = 0;
+
+
 //////////////////////////////////INCLUDED FILES
 
 #include "Vex_Competition_Includes.c"   //VEX REQUIRED FILE
@@ -57,10 +61,98 @@ float liftTarget = 0;
 
 ///////////////////////////////////////////////////BEGIN PREAUTONOMOUS
 
+
 void pre_auton()
 {
-	//#include "PreAuto.c"
-	//bStopTasksBetweenModes = true;
+  // Set bStopTasksBetweenModes to false if you want to keep user created tasks running between
+  // Autonomous and Tele-Op modes. You will need to manage all user created tasks if set to false.
+  bStopTasksBetweenModes = false;
+
+  	//DISPLAY BATTERY LEVEL
+
+	/*bLCDBacklight = true;                                    // Turn on LCD Backlight
+	string mainBattery;
+
+	for(int i = 0; i < 10; i ++)                                                        // An infinite loop to keep the program running until you terminate it
+	{
+		clearLCDLine(0);                                            // Clear line 1 (0) of the LCD
+		clearLCDLine(1);                                            // Clear line 2 (1) of the LCD
+
+		//Display the Primary Robot battery voltage
+		displayLCDString(0, 0, "Primary: ");
+		sprintf(mainBattery, "%1.2f%c", nImmediateBatteryLevel/1000.0,'V'); //Build the value to be displayed
+		displayNextLCDString(mainBattery);
+
+		//Short delay for the LCD refresh rate
+		wait1Msec(100);
+	}*/
+
+  	//SELECT MODE
+  	/*
+	while(vrDisabled && nLCDButtons != CENTER_BUTTON)
+	{
+		clearLCDLine(0);
+		clearLCDLine(1);
+		switch(autonomous_side)
+		{
+			case 0:
+				displayLCDString(0, 2, "Select Side:");
+				break;
+			case BLUE:
+				displayLCDString(0, 1, "BLUE Selected");
+				break;
+			case RED:
+				displayLCDString(0, 2, "RED Selected");
+				break;
+		}
+
+		if(nLCDButtons == LEFT_BUTTON)autonomous_side = BLUE;
+		else if(nLCDButtons == RIGHT_BUTTON)autonomous_side = RED;
+		displayLCDString(1, 0, "BLUE         RED");
+		wait1Msec(100);
+	}*/
+
+    //CALIBRATE GYRO
+
+	SensorValue[ConveyorPos] = 0;
+	clearLCDLine(0);
+	clearLCDLine(1);
+	displayLCDString(0, 0, "Calibrating Gyro");
+	SensorType[gyro] = sensorNone;
+	wait1Msec(250);
+	displayLCDString(1, 2, "*");
+	wait1Msec(250);
+	displayLCDString(1, 2, "**");
+	wait1Msec(250);
+	displayLCDString(1, 2, "***");
+	wait1Msec(250);
+	displayLCDString(1, 2, "****");
+	SensorType[gyro] = sensorGyro;
+	wait1Msec(250);
+	displayLCDString(1, 2, "*****");
+	wait1Msec(250);
+	displayLCDString(1, 2, "******");
+	wait1Msec(250);
+	displayLCDString(1, 2, "*******");
+	wait1Msec(250);
+	displayLCDString(1, 2, "********");
+	wait1Msec(250);
+	displayLCDString(1, 2, "*********");
+	wait1Msec(250);
+	displayLCDString(1, 2, "**********");
+	wait1Msec(250);
+	displayLCDString(1, 2, "***********");
+	wait1Msec(250);
+	displayLCDString(1, 2, "************");
+
+	clearLCDLine(0);
+	clearLCDLine(1);
+	displayLCDString(0, 0, "Gyro Calibrated!");
+	wait1Msec(1000);
+
+	StartTask(Lift_Control);
+	StartTask(Set_Drive);
+	StartTask(gyro_Drive);
 }
 
 ///////////////////////////////////////////////////BEGIN AUTONOMOUS
