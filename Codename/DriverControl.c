f3fbeefa0b9ae5d5827d3d5ab8bd2318007814dd
Henry Williams <hwill12345@gmail.com>
2017-01-21 01:15:12 -0500

    rename

--- Codename/DriverControl.c
+++ Codename/DriverControl.c
@@ -24,103 +24,47 @@ task usercontrol()
 	while (true)
 	{
 
-    if(vexRT[Btn6U])
-    {
-    	//liftTarget = LIFT_MAX;
-    	liftSpeed = -127;
-    }
-    else if(vexRT[Btn6D])
-    {
-    	//liftTarget = LIFT_MIN;
-    	liftSpeed = 127;
-    }
-    else if(!vexRT[Btn6U] && !vexRT[Btn6D])
-    {
-    	//liftTarget = LIFT_MIN;
-    	liftSpeed = 0;
-    }
+        if(vexRT[Btn6U])
+        {
+    	   //liftTarget = LIFT_MAX;
+    	   liftSpeed = -127;
+        }
+        else if(vexRT[Btn6D])
+        {
+    	   //liftTarget = LIFT_MIN;
+    	   liftSpeed = 127;
+        }
+        else if(!vexRT[Btn6U] && !vexRT[Btn6D])
+        {
+    	   //liftTarget = LIFT_MIN;
+    	   liftSpeed = 0;
+        }
 
-    if (vexRT[Btn5U] == 1)
-    {
-    	grabberSpeed = 127;
-    }
-    else if (vexRT[Btn5D] == 1)
-    {
-    	grabberSpeed = -127;
-    }
-    else
-    {
-     	grabberSpeed = 0;
-    }
+        if (vexRT[Btn5U] == 1)
+        {
+    	   grabberSpeed = 127;
+        }
+        else if (vexRT[Btn5D] == 1)
+        {
+    	   grabberSpeed = -127;
+        }
+        else
+        {
+     	  grabberSpeed = 0;
+        }
 
-    grabberRightSpeed = grabberSpeed;
-    grabberLeftSpeed = grabberSpeed;
-    motor[rightGrabber] = grabberRightSpeed;
-    motor[leftGrabber] = grabberLeftSpeed;
+        grabberRightSpeed = grabberSpeed;
+        grabberLeftSpeed = grabberSpeed;
+        motor[rightGrabber] = grabberRightSpeed;
+        motor[leftGrabber] = grabberLeftSpeed;
 
-    liftRightSpeed = liftSpeed;
-    liftLeftSpeed = liftSpeed;
-    motor[rightLift] = liftRightSpeed;
-    motor[leftLift] = liftLeftSpeed;
-    motor[rightLift2] = liftRightSpeed;
-    motor[leftLift2] = liftLeftSpeed;
+        liftRightSpeed = liftSpeed;
+        liftLeftSpeed = liftSpeed;
+        motor[rightLift] = liftRightSpeed;
+        motor[leftLift] = liftLeftSpeed;
+        motor[rightLift2] = liftRightSpeed;
+        motor[leftLift2] = liftLeftSpeed;
 
-    DrivePower(vexRT[Ch3] + vexRT[Ch1], vexRT[Ch3] - vexRT[Ch1]);
-
-    if(vexRT[Btn7L])
-    {
-    	MoveForDistance(DIST_TO_CUBE_Y);
-    }
-
-    if(vexRT[Btn7R])
-    {
-    	gyroTurningActive = true;
-	encoderDrivingActive = true;
-
-	MoveForDistance(DIST_TO_CUBE_Y);
-
-	gyroTarget = -950;
-	wait1Msec(2000);
-
-	MoveForDistance(DIST_TO_CUBE_X);
-
-	//GRAB CUBE
-	motor[rightGrabber] = -100;
-	motor[leftGrabber] = -100;
-	wait1Msec(1500);
-	motor[rightGrabber] = 0;
-	motor[leftGrabber] = 0;
-
-	//LIFT CUBE SLIGHTLY
-	motor[rightLift] = -50;
-	motor[leftLift] = -50;
-
-	gyroTarget = 1900;
-	wait1Msec(2000);
-
-	MoveForDistance(DIST_TO_CUBE_X);
-
-	gyroTarget = -950;
-	wait1Msec(2000);
-
-	MoveForDistance(DIST_TO_FENCE_FROM_CUBE_Y);
-
-	//LIFT CUBE
-	motor[rightLift] = -127;
-	motor[leftLift] = -127;
-	wait1Msec(1500);
-	motor[rightLift] = 0;
-	motor[leftLift] = 0;
-
-	//LET GO OF CUBE
-	motor[rightGrabber] = 100;
-	motor[leftGrabber] = 100;
-	wait1Msec(1500);
-	motor[rightGrabber] = 0;
-	motor[leftGrabber] = 0;
-
-	encoderDrivingActive = false;
-	gyroTurningActive = false;
-    }
+        DrivePower(vexRT[Ch3] + vexRT[Ch1], vexRT[Ch3] - vexRT[Ch1]);
 	}
 }
